d02635a3829269c0465f2826f3971afb7832dc2d
Yway Chit Aung <ywaychitaung@gmail.com>
2021-12-14 23:36:44 +0630

    Tweak footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -10,73 +10,105 @@
                 </svg>
                 <span class="ml-4 text-xl font-semibold">Ecommerce</span>
             </a>
-            <p class="mt-2 text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, porro.</p>
+            <p class="mt-8 text-gray-700 text-sm font-semibold leading-6">This is a demo project, built with 
+                <a href="https://laravel.com" target="_blank" rel="noopener noreferrer" class="hover:text-[#FF2D20]">Laravel</a>, 
+                <a href="https://v3.vuejs.org" target="_blank" rel="noopener noreferrer" class="hover:text-[#41B883]">Vue.js</a> and 
+                <a href="https://tailwindcss.com" target="_blank" rel="noopener noreferrer" class="hover:text-[#07B6D5]">Tailwind CSS</a>.
+            </p>
         </div>
 
         <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 class="text-gray-800 mb-4">CATEGORIES</h2>
+                <h2 class="text-gray-900 font-semibold mb-4">About</h2>
 
                 <nav class="space-y-4">
-                    <a class="block hover:text-gray-800">First Link</a>
+                    <a href="#" class="block hover:text-gray-800">Company Info</a>
                     
-                    <a class="block hover:text-gray-800">Second Link</a>
+                    <a href="#" class="block hover:text-gray-800">News</a>
                     
-                    <a class="block hover:text-gray-800">Third Link</a>
-                    
-                    <a class="block hover:text-gray-800">Fourth Link</a>
+                    <a href="#" class="block hover:text-gray-800">Terms & Conditions</a>
+
+                    <a href="#" class="block hover:text-gray-800">Privacy Policy</a>
                 </nav>
             </div>
 
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 class="text-gray-800 mb-4">CATEGORIES</h2>
+                <h2 class="text-gray-900 font-semibold mb-4">Help & Contact</h2>
 
                 <nav class="space-y-4">
-                    <a class="block hover:text-gray-800">First Link</a>
+                    <a href="#" class="block hover:text-gray-800">Help Center</a>
                     
-                    <a class="block hover:text-gray-800">Second Link</a>
+                    <a href="#" class="block hover:text-gray-800">Contact Us</a>
                     
-                    <a class="block hover:text-gray-800">Third Link</a>
-                    
-                    <a class="block hover:text-gray-800">Fourth Link</a>
+                    <a href="#" class="block hover:text-gray-800">Ask Refund</a>
                 </nav>
             </div>
 
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 class="text-gray-800 mb-4">CATEGORIES</h2>
+                <h2 class="text-gray-900 font-semibold mb-4">Social</h2>
 
                 <nav class="space-y-4">
-                    <a class="block hover:text-gray-800">First Link</a>
+                    <div class="flex items-center hover:text-[#1877F2] space-x-2">
+                        <!-- Logo: Facebook -->
+                        <svg class="h-4 w-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M12,2C6.477,2,2,6.477,2,12c0,5.013,3.693,9.153,8.505,9.876V14.65H8.031v-2.629h2.474v-1.749 c0-2.896,1.411-4.167,3.818-4.167c1.153,0,1.762,0.085,2.051,0.124v2.294h-1.642c-1.022,0-1.379,0.969-1.379,2.061v1.437h2.995 l-0.406,2.629h-2.588v7.247C18.235,21.236,22,17.062,22,12C22,6.477,17.523,2,12,2z"/>
+                        </svg>
+                        <a href="#" class="block">Facebook</a>
+                    </div>
                     
-                    <a class="block hover:text-gray-800">Second Link</a>
-                    
-                    <a class="block hover:text-gray-800">Third Link</a>
-                    
-                    <a class="block hover:text-gray-800">Fourth Link</a>
+                    <div class="flex items-center hover:text-[#0096F4] space-x-2">
+                        <!-- Logo: Twitter -->
+                        <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                            <path fill="currentColor" d="M42,12.429c-1.323,0.586-2.746,0.977-4.247,1.162c1.526-0.906,2.7-2.351,3.251-4.058c-1.428,0.837-3.01,1.452-4.693,1.776C34.967,9.884,33.05,9,30.926,9c-4.08,0-7.387,3.278-7.387,7.32c0,0.572,0.067,1.129,0.193,1.67c-6.138-0.308-11.582-3.226-15.224-7.654c-0.64,1.082-1,2.349-1,3.686c0,2.541,1.301,4.778,3.285,6.096c-1.211-0.037-2.351-0.374-3.349-0.914c0,0.022,0,0.055,0,0.086c0,3.551,2.547,6.508,5.923,7.181c-0.617,0.169-1.269,0.263-1.941,0.263c-0.477,0-0.942-0.054-1.392-0.135c0.94,2.902,3.667,5.023,6.898,5.086c-2.528,1.96-5.712,3.134-9.174,3.134c-0.598,0-1.183-0.034-1.761-0.104C9.268,36.786,13.152,38,17.321,38c13.585,0,21.017-11.156,21.017-20.834c0-0.317-0.01-0.633-0.025-0.945C39.763,15.197,41.013,13.905,42,12.429"/>
+                        </svg>
+                        <a href="#" class="block">Twitter</a>
+                    </div>
+
+                    <div class="flex items-center hover:text-[#FF0000] space-x-2">
+                        <!-- Logo: Youtube -->
+                        <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                            <path fill="currentColor" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"/>
+                            <path fill="#FFFFFF" d="M20 31L20 17 32 24z"/>
+                        </svg>
+                        <a href="#" class="block">Youtube</a>
+                    </div>
                 </nav>
             </div>
 
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 class="text-gray-800 mb-4">CATEGORIES</h2>
-
-                <nav class="space-y-4">
-                    <a class="block hover:text-gray-800">First Link</a>
-                    
-                    <a class="block hover:text-gray-800">Second Link</a>
-                    
-                    <a class="block hover:text-gray-800">Third Link</a>
-                    
-                    <a class="block hover:text-gray-800">Fourth Link</a>
-                </nav>
+                <div class="space-y-8">
+                    <button class="bg-gray-200 inline-flex py-3 px-5 rounded-lg items-center focus:outline-none">
+                        <!-- Logo: Google Play Store -->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-6 w-6" viewBox="0 0 512 512">
+                            <path d="M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z"></path>
+                        </svg>
+                        <span class="ml-4 flex items-start flex-col">
+                            <span class="text-xs text-gray-600 mb-1">GET IT ON</span>
+                            <span class="title-font font-medium">Google Play</span>
+                        </span>
+                    </button>
+    
+                    <button class="bg-gray-200 inline-flex py-3 px-5 rounded-lg items-center focus:outline-none">
+                        <!-- Logo: App Store -->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-6 w-6" viewBox="0 0 305 305">
+                            <path d="M40.74 112.12c-25.79 44.74-9.4 112.65 19.12 153.82C74.09 286.52 88.5 305 108.24 305c.37 0 .74 0 1.13-.02 9.27-.37 15.97-3.23 22.45-5.99 7.27-3.1 14.8-6.3 26.6-6.3 11.22 0 18.39 3.1 25.31 6.1 6.83 2.95 13.87 6 24.26 5.81 22.23-.41 35.88-20.35 47.92-37.94a168.18 168.18 0 0021-43l.09-.28a2.5 2.5 0 00-1.33-3.06l-.18-.08c-3.92-1.6-38.26-16.84-38.62-58.36-.34-33.74 25.76-51.6 31-54.84l.24-.15a2.5 2.5 0 00.7-3.51c-18-26.37-45.62-30.34-56.73-30.82a50.04 50.04 0 00-4.95-.24c-13.06 0-25.56 4.93-35.61 8.9-6.94 2.73-12.93 5.09-17.06 5.09-4.64 0-10.67-2.4-17.65-5.16-9.33-3.7-19.9-7.9-31.1-7.9l-.79.01c-26.03.38-50.62 15.27-64.18 38.86z"></path>
+                            <path d="M212.1 0c-15.76.64-34.67 10.35-45.97 23.58-9.6 11.13-19 29.68-16.52 48.38a2.5 2.5 0 002.29 2.17c1.06.08 2.15.12 3.23.12 15.41 0 32.04-8.52 43.4-22.25 11.94-14.5 17.99-33.1 16.16-49.77A2.52 2.52 0 00212.1 0z"></path>
+                        </svg>
+                        <span class="ml-4 flex items-start flex-col">
+                            <span class="text-xs text-gray-600 mb-1">Download on the</span>
+                            <span class="title-font font-medium">App Store</span>
+                        </span>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="text-gray-800 md:px-16 pb-12">
+    <div class="text-gray-900 md:px-16 pb-12">
         <div class="flex flex-col md:flex-row items-center justify-center md:justify-between">
             <span>Copyright © {{ date('Y') }} Ecommerce Company Inc.</span>
 
-            <a href="https://ywaychitaung.dev" rel="noopener noreferrer" target="_blank" class="tracking-wide">@ywaychitaung</a>
+            <a href="https://www.ywaychitaung.com" target="_blank" rel="noopener noreferrer" class="tracking-wide">@ywaychitaung</a>
         </div>
     </div>
 </footer>
