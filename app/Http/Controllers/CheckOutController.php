9d9349f24c304cd9b1eedf233d3f932c9eb00593
Yway Chit Aung <ywaychitaung@gmail.com>
2021-12-16 20:18:02 +0630

    Tweak purchased.blade.php

--- app/Http/Controllers/CheckOutController.php
+++ app/Http/Controllers/CheckOutController.php
@@ -29,7 +29,7 @@ class CheckOutController extends Controller
         $checkout_session = Stripe\Checkout\Session::create([
             'line_items' => [[ $products ]],
             'mode' => 'payment',
-            'success_url' => env('APP_URL').'/purchased',
+            'success_url' => env('APP_URL').'/purchased?session_id={CHECKOUT_SESSION_ID}',
             'cancel_url' => env('APP_URL').'/cart',
         ]);
 
@@ -40,6 +40,18 @@ class CheckOutController extends Controller
     {
         Cart::destroy();
 
-        return view('purchased');
+        // Set Stripe API key
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+
+        // Get session_id from URL (e.g. http://example.com/purchased?session_id=cs_test_123)
+        $session_id = request('session_id');
+
+        // Retrieve the Checkout Session
+        $session = Stripe\Checkout\Session::retrieve($session_id);
+
+        // Get the customer's data from the session
+        $customer = Stripe\Customer::retrieve($session->customer);
+
+        return view('purchased', compact('customer'));
     }
 }
--- resources/views/purchased.blade.php
+++ resources/views/purchased.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app', ['title' => 'Order Purchased'])
 
 @section('content')
-    <p>Purchased</p>
+    <div class="w-2/5 bg-blue-50 text-center rounded-2xl mx-auto mt-6 mb-16 py-16">
+        <p>Thank you, <span class="font-semibold">{{ $customer->name }}</span>.</p>
+        <p>Your payment is successful.</p>
+        <p>We'll send an invoice to <span class="font-semibold">{{ $customer->email }}</span>.</p>
+
+        <div class="flex justify-center mt-8">
+            <a href="{{ route('home') }}" class="button">
+                Buy More
+            </a>
+        </div>
+    </div>
 @endsection
