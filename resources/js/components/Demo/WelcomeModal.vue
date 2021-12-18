d31235698ffa8d0c13763bfc42c2be0ef4ba3552
Yway Chit Aung <ywaychitaung@gmail.com>
2021-12-18 12:11:12 +0630

    Update modal

--- resources/js/components/Demo/WelcomeModal.vue
+++ resources/js/components/Demo/WelcomeModal.vue
@@ -37,10 +37,6 @@
                     <button @click="open = false" type="button" class="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white hover:bg-blue-500 text-base font-medium text-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                         Okay, got it!
                     </button>
-
-                    <button @click="open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                        I won't tell you. Never!
-                    </button>
                 </div>
             </div>
         </div>
