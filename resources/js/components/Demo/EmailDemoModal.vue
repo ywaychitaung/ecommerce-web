b54ea124cae0217c6409ec86084d2cecf3a93b5d
Yway Chit Aung <ywaychitaung@gmail.com>
2021-12-22 15:53:47 +0630

    Add email demo modal

--- resources/js/components/Demo/EmailDemoModal.vue
+++ resources/js/components/Demo/EmailDemoModal.vue
@@ -1,6 +1,4 @@
 <template>
-    <button @click="open = true" class="button w-full">Checkout</button>
-
     <div v-show="open" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
@@ -11,44 +9,33 @@
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <div class="sm:flex sm:items-start">
                         <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-                            <!-- Heroicons: credit-card -->
+                            <!-- Heroicons: mail -->
                             <svg class="h-6 w-6 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
                         </div>
 
                         <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                             <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                                Payment is powered by <span class="text-[#7632FF]">Stripe</span> (Test Mode).
+                                Email service is powered by <span class="text-[#FF5959]">Mailgun</span> (Test Mode).
                             </h3>
 
                             <div class="text-sm text-gray-700 leading-6 mt-2">
                                 <p>
-                                    I'm using Stripe's Prebuilt Checkout page.
-                                    <span class="text-red-500">For any reason, don't enter your credit/debit card details. I won't take responsibility for that.</span>
+                                    I'm using Mailgun's Sandbox subdomain for testing. You have to contact me before you continue.
                                 </p>
 
-                                <p class="mt-4">Here is the testing card. You can test with it.</p>
-
-                                <p class="mt-4">Card Number: 4242 4242 4242 4242</p>
-                                <p class="mt-2">Expire Date: 06/{{ (new Date().getFullYear() + 3).toString().substring(2) }}</p>
-                                <p class="mt-2">CVC: 123</p>
+                                <p class="mt-4">Email: <span class="text-blue-500">yca@duck.com</span></p>
+                                <p class="mt-2">Telegram: <a href="https://t.me/ywaychitaung" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">https://t.me/ywaychitaung</a></p>
+                                <p class="mt-2">Check My Website: <a href="https://www.ywaychitaung.com" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">https://www.ywaychitaung.com</a></p>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <form action="checkout" method="POST">
-                        <input type="hidden" name="_token" :value="csrf">
-
-                        <button type="submit" class="modal-button">
-                            Proceed to Payment
-                        </button>
-                    </form>
-
-                    <button @click="open = false" type="button" class="modal-cancel-button">
-                        Cancel
+                    <button @click="open = false" type="button" class="modal-button">
+                        Okay, got it!
                     </button>
                 </div>
             </div>
@@ -61,11 +48,9 @@ import { ref } from 'vue'
 
 export default {
     setup() {
-        const open = ref(false)
-        const csrf = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        const open = ref(true)
 
         return {
-            csrf,
             open
         }
     }
--- resources/views/payment-success.blade.php
+++ resources/views/payment-success.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app', ['title' => 'Payment Success'])
 
 @section('content')
+    <email-demo-modal></email-demo-modal>
+
     <div class="w-2/5 bg-blue-100 text-center rounded-2xl mx-auto mt-6 mb-16 py-16">
         <p>Thank you, <span class="font-semibold">{{ $customer->name }}</span>.</p>
         <p>Your payment is successful.</p>
